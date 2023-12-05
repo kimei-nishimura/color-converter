47e405e6d93522304655735546deead35324b158
kimei-nishimura <85261090+kimei-nishimura@users.noreply.github.com>
2023-12-04 22:05:25 -0600

    updated but but still not fully right.

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
-color-converter script
+# color-converter script
 
 based on code from
 https://codingartistweb.com/2021/08/rgb-to-hex-hex-to-rgb-converter-with-javascript/
-
--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 let hexInput = document.getElementById("hex");
 let rgbInput = document.getElementById("rgb");
+let lslInput = document.getElementById("lsl");
+let hslInput = document.getElementById("hsl");
 
 window.onload=()=>{
     hexInput.value = "";
     rgbInput.value = "";
+    lslInput.value = "";
+    hslInput.value = "";
 }
 
 function valid(element){
@@ -15,7 +19,7 @@ function invalid(element,otherElement){
     otherElement.value = 0;
 }
 
-function toRgb(){
+function HEXtoRGB(){
     let hexCode = hexInput.value;
     let rgbArr = [];
     if(/^#?[A-Fa-f0-9]{6}$/.test(hexCode)){
@@ -33,7 +37,7 @@ function toRgb(){
     }
 }
 
-function toHex(){
+function RGBtoHEX(){
     let rgbCode = rgbInput.value;
     let rgbRegex1 = /^rgb\([0-9]{1,3},[0-9]{1,3},[0-9]{1,3}\)$/;
     let rgbRegex2 = /^[0-9]{1,3},[0-9]{1,3},[0-9]{1,3}$/
@@ -60,5 +64,110 @@ function toHex(){
     else{
         invalid(rgbInput,hexInput);
     }
+}
 
+function RGBtoLSL(){
+    let rgbCode = rgbInput.value;
+    let rgbRegex1 = /^rgb\([0-9]{1,3},[0-9]{1,3},[0-9]{1,3}\)$/;
+    let rgbRegex2 = /^[0-9]{1,3},[0-9]{1,3},[0-9]{1,3}$/
+    let lslArr=[];
+    if(rgbRegex1.test(rgbCode) || rgbRegex2.test(rgbCode)){
+        rgbCode = rgbCode.replace(/[rgb()]+/g,"") || rgbCode;
+        rgbCode = rgbCode.split(",");
+        let condition = rgbCode.every((value) => {
+            return parseInt(value) <= 255;
+        });
+        if(condition){
+            valid(rgbInput);
+            rgbCode.forEach(value => {
+                lslArr.push((parseInt(value)/255).toFixed(3));
+                console.log(lslArr);
+            });
+            lslInput.value = "lsl(" + lslArr + ")";
+        }
+        else {
+            invalid(lslInput,rgbInput)
+        }
+    }
+}
+
+function RGBtoHSL(){
+    let rgbCode = rgbInput.value;
+    let rgbRegex1 = /^rgb\([0-9]{1,3},[0-9]{1,3},[0-9]{1,3}\)$/;
+    let rgbRegex2 = /^[0-9]{1,3},[0-9]{1,3},[0-9]{1,3}$/
+    let hslArr=[];
+    if(rgbRegex1.test(rgbCode) || rgbRegex2.test(rgbCode)){
+        rgbCode = rgbCode.replace(/[rgb()]+/g,"") || rgbCode;
+        rgbCode = rgbCode.split(",");
+        let condition = rgbCode.every((value) => {
+            return parseInt(value) <= 255;
+        });
+        if(condition){
+            valid(rgbInput);
+            const max = Math.max(...rgbCode);
+            const min = Math.min(...rgbCode);
+            const lum = (max + min) / 2;
+            if (max === min) {
+                hue = 0;
+                sat = 0;
+            } else {
+                const d = max - min;
+                const sat = lum > 0.5 ? d / (2 - max - min) : d / (max + min);
+                r = rgbCode.at(0);
+                g = rgbCode.at(1);
+                b = rgbCode.at(2);
+                let hue;
+                switch (max) {
+                    case r:
+                        hue = ((g - b) / d + (g < b ? 6 : 0)) / 6;
+                        break;
+                    case g:
+                        hue = ((b - r) / d + 2) / 6;
+                        break;
+                    case b:
+                        hue = ((r - g) / d + 4) / 6;
+                        break;
+                    default:
+                        hue = 0;
+                        break;
+                }
+                hslArr[0] = hue;
+                hslArr[1] = sat;
+                hslArr[2] = lum;
+            }
+            hslInput.value = `hsl(${Math.round(hslArr[0] * 360)}, ${Math.round(hslArr[1] * 100)}%, ${Math.round(hslArr[2] * 100)}%)`;
+//            hslInput.value = "hsl(" + hslArr + ")";
+            console.log(hslArr);
+        }
+    }
+}
+
+// Example usage:
+const rgbColor = { r: 255, g: 0, b: 0 }; // Red color
+const hslColor = RGBtoHSL(rgbColor.r, rgbColor.g, rgbColor.b);
+const lslColor = RGBtoLSL(rgbColor.r, rgbColor.g, rgbColor.b);
+console.log(hslColor);
+
+function fromRgb(){
+    RGBtoHEX();
+    RGBtoLSL();
+    RGBtoHSL();
+}
+
+function fromHex(){
+    HEXtoRGB();
+    RGBtoLSL();
+    RGBtoHSL();
+}
+
+function fromLsl(){
+    LSLtoRGB();
+    RGBtoHEX();
+    RGBtoHSL();
+}
+
+function fromHsl(){
+    HSLtoRGB();
+    RGBtoHEX();
+    RGBtoLSL();
 }
